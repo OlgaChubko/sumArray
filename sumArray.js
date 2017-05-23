74d334cd2269f1033319196b358a793d6f16a563
olga <olga.chubko.92@mail.ru>
2017-05-23 18:05:31 +0300

    first commit

--- sumArray.js
+++ sumArray.js
@@ -1,7 +1,5 @@
 //array = [1, 2, [3, 4, [5, 6], 7], 8, [9, 10]];
 
-
-
 var array = [1, 2, [3, 4],5, 6, 7, 8, [9, 10]];
 var concat = array.reduce(function(result, current) {
     return result.concat(current);
